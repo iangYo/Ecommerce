02d4548956784f2fd6773af89c2436403e7942f2
iangYo <iangabrielrodrigues@gmail.com>
2020-02-28 14:52:03 -0300

    criado validações para controller categorias e rotas categorias atualizadas para validações

--- api/routes/api/v1/categorias.js
+++ api/routes/api/v1/categorias.js
@@ -1,4 +1,4 @@
-const router = requires('express').Router();
+const router = require('express').Router();
 
 const CategoriaController = require('../../../controllers/CategoriaController');
 
@@ -9,12 +9,12 @@ const { CategoriaValidation } = require('../../../controllers/validations/catego
 
 const categoriaController = new CategoriaController();
 
-router.get('/', categoriaController.index);
-router.get('/disponiveis', categoriaController.indexDisponiveis);
-router.get('/:id', categoriaController.show);
+router.get('/', Validation(CategoriaValidation.index), categoriaController.index);
+router.get('/disponiveis', Validation(CategoriaValidation.indexDisponiveis), categoriaController.indexDisponiveis);
+router.get('/:id', Validation(CategoriaValidation.show), categoriaController.show);
 
-router.post('/', auth.required, LojaValidation.admin, categoriaController.store);
-router.put('/:id', auth.required, LojaValidation.admin, categoriaController.update);
-router.delete('/:id', auth.required, LojaValidation.admin, categoriaController.remove);
+router.post('/', auth.required, LojaValidation.admin, Validation(CategoriaValidation.store), categoriaController.store);
+router.put('/:id', auth.required, LojaValidation.admin, Validation(CategoriaValidation.update), categoriaController.update);
+router.delete('/:id', auth.required, LojaValidation.admin, Validation(CategoriaValidation.remove), categoriaController.remove);
 
 module.exports = router;
