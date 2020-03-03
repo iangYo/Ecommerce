b2fcd784ca68c14b2db8228ce0588a9face7fb27
iangYo <iangabrielrodrigues@gmail.com>
2020-03-03 16:02:19 -0300

    validações para produtos

--- api/routes/api/v1/produtos.js
+++ api/routes/api/v1/produtos.js
@@ -1,24 +1,26 @@
 const router = require('express').Router();
 
-const ProdutoController = require('../../../controllers/ProdutoController2');
+const ProdutoController = require('../../../controllers/ProdutoController');
 
 const { LojaValidation } = require('../../../controllers/validations/lojaValidation');
+const Validation = require('express-validation');
+const { ProdutoValidation } = require('../../../controllers/validations/produtoValidation');
 const auth = require('../../auth');
 const upload = require('../../../config/multer');
 
 const produtoController = new ProdutoController();
 
 // ADMIN
-router.post('/', auth.required, LojaValidation.admin, produtoController.store);
-router.put(':id', auth.required, LojaValidation.admin, produtoController.update);
-router.put('/images/:id', auth.required, LojaValidation.admin, upload.array('files', 4), produtoController.updateImages);
+router.post('/', auth.required, LojaValidation.admin, Validation(ProdutoValidation.store), produtoController.store);
+router.put(':id', auth.required, LojaValidation.admin, Validation(ProdutoValidation.update), produtoController.update);
+router.put('/images/:id', auth.required, LojaValidation.admin, Validation(ProdutoValidation.updateImages), upload.array('files', 4), produtoController.updateImages);
 router.delete('/:id', auth.required, LojaValidation.admin, produtoController.remove);
 
 // CLIENT/GUEST
 router.get('/', produtoController.index);
-router.get('/disponiveis', produtoController.indexDisponiveis);
-router,get('/search/:search', produtoController.search);
-router.get('/:id', produtoController.show); 
+router.get('/disponiveis', Validation(ProdutoValidation.indexDisponiveis), produtoController.indexDisponiveis);
+router.get('/search/:search', Validation(ProdutoValidation.search), produtoController.search);
+router.get('/:id', Validation(ProdutoValidation.show), produtoController.show);
 
 // VARIACOES
 
