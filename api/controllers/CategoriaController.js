62d5a80407c66ad75f393f2afa2e0618cbb87732
iangYo <iangabrielrodrigues@gmail.com>
2020-03-06 13:43:44 -0300

    métodos das rotas relacionadas ao produto no arquivo categoria adicionados no categoriaController

--- api/controllers/CategoriaController.js
+++ api/controllers/CategoriaController.js
@@ -6,43 +6,43 @@ class CategoriaController {
 
     // GET / index
     index(req, res, next) {
-        Categoria.find({ loja: req.query.loja })
+        Categoria.find({loja: req.query.loja})
             .select('_id produtos nome codigo loja')
-            .then(categorias => res.send({ categorias }))
+            .then(categorias => res.send({categorias}))
             .catch(next);
     }
 
     // GET /disponiveis
     indexDisponiveis(req, res, next) {
-        Categoria.find({ loja: req.query.loja, disponibilidade: true })
+        Categoria.find({loja: req.query.loja, disponibilidade: true})
             .select('_id produtos nome codigo loja')
-            .then(categorias => res.send({ categorias }))
+            .then(categorias => res.send({categorias}))
             .catch(next);
     }
 
     // GET /:id show
     show(req, res, next) {
-        Categoria.findOne({ loja: req.query.loja, _id: req.params.id })
+        Categoria.findOne({loja: req.query.loja, _id: req.params.id})
             .select('_id produtos nome codigo loja')
             .populate(['produtos'])
-            .then(categoria => res.send({ categoria }))
+            .then(categoria => res.send({categoria}))
             .catch(next);
     }
 
     //  POST / store
     store(req, res, next) {
-        const { nome, codigo } = req.body;
-        const { loja } = req.query;
+        const {nome, codigo} = req.body;
+        const {loja} = req.query;
 
-        const categoria = new Categoria({ nome, codigo, loja, disponibilidade: true });
+        const categoria = new Categoria({nome, codigo, loja, disponibilidade: true});
         categoria.save()
-            .then(() => res.send({ categoria }))
+            .then(() => res.send({categoria}))
             .catch(next);
     }
 
     // PUT /:id update
     async update(req, res, next) {
-        const { nome, codigo, disponibilidade, produtos } = req.body;
+        const {nome, codigo, disponibilidade, produtos} = req.body;
         try {
             const categoria = await Categoria.findById(req.params.id);
 
@@ -52,7 +52,7 @@ class CategoriaController {
             if (produtos) categoria.produtos = produtos;
 
             await categoria.save();
-            return res.send({ categoria });
+            return res.send({categoria});
         } catch (e) {
             next(e);
         }
@@ -72,7 +72,55 @@ class CategoriaController {
     /**
      * PRODUTOS
      */
-    
+    // GET /:id/produtos - showProdutos
+    async showProdutos(req, res, next) {
+        try {
+            const { offset, limit } = req.query;
+            const produtos = await Produtos.paginate(
+                { categoria: req.params.id },
+                { offset: Number(offset) || 0, limit: Number(limit) || 30 }
+            );
+            return res.send({ produtos });
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    // PUT /:id/produtos - updateProdutos
+    async updateProdutos(req, res, next) {
+        try {
+            const categoria = await Categoria.findById(req.params.id);
+            const { produtos } = req.body;
+            if (produtos) categoria.produtos = produtos;
+            await categoria.save();
+
+            let _produtos = await Produto.find({
+                $or: [
+                    { categoria: req.params.id },
+                    { _id: { $in: produtos } }
+                ]
+            });
+
+            _produtos = await Promise.all(_produtos.map(async (produto) => {
+                if (!produto.include(produto._id.toString())) {
+                    produto.categoria = null;
+                } else {
+                    produto.categoria = req.params.id;
+                }
+                await produto.save();
+                return produto;
+            }));
+
+            const resultados = await Produtos.paginate(
+                { categoria: req.params.id },
+                { offset: 0, limit: 30 }
+            );
+
+            return res.send({ produtos: resultados });
+        } catch (e) {
+            next(e);
+        }
+    }
 }
 
 module.exports = CategoriaController;
