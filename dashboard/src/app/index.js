04bc51b2169e89a621482d2e92a9bcb06913af25
iangYo <iangabrielrodrigues@gmail.com>
2020-04-27 16:52:30 -0300

    instalado sistema de rotas

--- dashboard/src/app/index.js
+++ dashboard/src/app/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
+
 import { Provider } from 'react-redux';
 import store from './store';
+
+import { HashRouter as Router, Route } from 'react-router-dom';
+
+import Pedidos from './containers/Pedidos';
 
 function App() {
   return (
     <Provider store={store}>
-      <div className='App'>
-        Dashboard
-      </div>
+      <Router>
+        <div className='App'>
+          <Route path={'/'} exact component={Pedidos} />
+        </div>
+      </Router>
     </Provider>
   );
 }
